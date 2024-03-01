403c53335c8be6af238825fdbd3a8ed4319f2a9b
Mark Fynes <tfynes@vmware.com>
2024-03-01 15:36:27 -0500

    .

---
+++ src/test/java/com/example/demopostgres/DemoPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
